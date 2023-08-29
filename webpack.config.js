6da6c8d5fa05af7a79281a76e56614e345e843e3
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-08-29 23:41:07 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    mode: 'development',
+    entry:'./src/index.js', // entry point,can be multiple too i.e "Code Splitting"
+
+
+
+    devtool: 'inline-source-map',
+
+    devServer: {
+        static: './dist',
+    },
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        
+
+    },
+
+    optimization: {
+       // runtimeChunk: 'single',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    
+                ],
+
+            },
+
+            {
+
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+
+            },
+
+            {
+
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+
+            },
+
+
+        ],
+
+
+    },
+
+    plugins: [
+        //new BundleAnalyzerPlugin()
+        
+    ],
+
+
+};
