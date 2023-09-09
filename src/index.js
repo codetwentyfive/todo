523b0e0e61e522f04a1576b80ffcad185700a56e
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-09-09 02:02:17 +0200

    feat:Moved DOM elements to dom.js and added info icon

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-import'./dom.js';
+import './dom.js';
 import './styles.css';
 import { updateTaskTitleStyle, activeSwitcher, toggleCheckboxOnClick } from './style.js';
 
-
-// DOM elements
-const newTaskButton = document.getElementById("newTaskButton");
-const taskForm = document.getElementById("taskForm");
-const taskFormElement = document.getElementById("taskFormElement");
-const taskList = document.getElementById("taskList");
-const modal = document.getElementById("modal");
 
 // Event listener for the "+ New Task" button
 newTaskButton.addEventListener("click", (e) => {
     // Prevent the click event from propagating to the document
     e.stopPropagation();
-    
+
     // Toggle the visibility of the task form and modal
     taskForm.classList.toggle("hidden");
     modal.classList.toggle("hidden");
@@ -23,31 +16,31 @@ newTaskButton.addEventListener("click", (e) => {
 // Event listener for the task form submission
 taskFormElement.addEventListener("submit", (e) => {
     e.preventDefault();
-    
+
     // Get values from the form
     const title = document.getElementById("title").value;
     const dueDate = document.getElementById("dueDate").value;
     const priority = document.getElementById("priority").value;
     const description = document.getElementById("description").value;
-    
+
     // Create a new task object
     const task = {
         title,
         dueDate,
         priority,
         description,
-        completed:false,
+        completed: false,
     };
-    
+
     // Save the task to localStorage
     saveTask(task);
-    
+
     // Add the task to the task list
     addTaskToTaskList(task);
-    
+
     // Clear the form
     taskFormElement.reset();
-    
+
     // Hide the form and modal
     taskForm.classList.add("hidden");
     modal.classList.add("hidden");
@@ -91,14 +84,14 @@ function saveTask(task) {
 function removeTask(taskItem) {
     // Remove the task item from the task list
     taskList.removeChild(taskItem);
-    
+
     // Get the list of tasks from localStorage
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    
+
     // Find and remove the task from the tasks array based on some unique identifier (e.g., task title)
     const titleToRemove = taskItem.querySelector("h3").textContent;
     const updatedTasks = tasks.filter((task) => task.title !== titleToRemove);
-    
+
     // Save the updated tasks back to localStorage
     localStorage.setItem("tasks", JSON.stringify(updatedTasks));
 }
@@ -113,10 +106,10 @@ function addTaskToTaskList(task) {
     taskItem.innerHTML = `
     <label class="task">
         <input type="checkbox" class="task-checkbox" ${task.completed ? 'checked' : ''}>
-        <h3>${task.title}</h3>
-        <p>Due Date: ${task.dueDate}</p>
-        <p>Priority: ${task.priority}</p>
-        <p>Description: ${task.description}</p>
+        <h3 class="task-title">${task.title}</h3>
+        <p class="due-date">${task.dueDate}</p>
+        <p>Urgency: <br>${task.priority}</p>
+        <svg class="info-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" /></svg>
         <button class="remove-task-button">❌</button>
     </label>
     `;
@@ -142,18 +135,15 @@ function addTaskToTaskList(task) {
     updateTaskTitleStyle(taskItem, task.completed);
 }
 
-
-
-
 // Function to load tasks from localStorage and display them
 function loadTasks() {
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
+    
     // Loop through the tasks and add them to the task list
     tasks.forEach((task) => {
         addTaskToTaskList(task);
         const taskItem = taskList.lastChild; // Get the reference to the added task item
-
+        
         // Set the checked attribute of the checkbox based on the task's completed status
         const checkbox = taskItem.querySelector(".task-checkbox");
         checkbox.checked = task.completed;
